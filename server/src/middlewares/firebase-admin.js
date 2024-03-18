65bfc91682987720d25f32170d804ef3e6d3e605
dnhai0311 <dnhai0311@gmail.com>
2024-03-18 19:22:32 +0700

    change api key firebase

--- server/src/middlewares/firebase-admin.js
+++ server/src/middlewares/firebase-admin.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../timtronhanh-ebce7-firebase-adminsdk-7ohdv-f96909d427.json');
+const serviceAccount = require('../../api_key/timtronhanh-ebce7-firebase-adminsdk-7ohdv-f96909d427.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
